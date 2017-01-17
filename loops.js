5d2200f700b6d1d62220ade94808a5b765856eb0
franco83-2 <franco.84@gmail.com>
2017-01-17 17:23:09 -0500

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop(a) {
+  for (let i = 0; i < 25; i++) {
+  a.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+  }
+  return a
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n)
+    n -= 1
+  }
+  return "done"
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(a) {
+  do {
+    a.pop()
+  } while (a.length > 0 && maybeTrue())
+  return a
+}
